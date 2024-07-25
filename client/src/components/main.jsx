19995e8356b25fdaec68afdba8ec15d0eb8c6e6a
Aivaz <160047470+Aivaz228@users.noreply.github.com>
2024-07-25 15:34:49 +0300

    css

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -27,7 +27,7 @@ const Main = () => {
   return (
     <div className={styles.wrap}>
       <div className={styles.container}>
-        <h1 className={styles.heading}>Join</h1>
+        <h1 className={styles.heading}>Общайся, пока ждешь заказ</h1>
 
         <form className={styles.form}>
           <div className={styles.group}>
